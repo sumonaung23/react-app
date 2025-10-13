ccff85ded317182227fa4327347dc899519c4675
sumonaung23 <sumonaung575@gmail.com>
2025-10-13 23:19:23 +0700

    completed card component for the name card that include styling

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,11 @@
-
+import Card from "./Card"
 
 function App() {
-  
+  return (
+    <>
+      <Card />
+    </>
+  );
 }
 
 export default App
---
+++ my-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card () {
+    return (
+        <div className="card">
+            <img src="https://i.pinimg.com/1200x/15/13/0e/15130e2056b09c1118484f8a47c71739.jpg" alt="profile picture" />
+            <h2>Su Mon Aung</h2>
+            <p>I am a Front-End Web Developer</p>
+        </div>
+    );
+}
+
+export default Card
